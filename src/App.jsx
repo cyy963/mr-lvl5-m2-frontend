7aab3c970b7b106ceb0ece011b955e0e9720c94d
cyrus_w <cyrusw89@gmail.com>
2024-05-27 15:36:08 +1200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+// import ImageUploader from './ImageUploader.jsx';
+import './App.css';
+
+import TCnav from './assets/TCnav.png';
+import TCbanner from './assets/TCbanner.png';
+import TCfooter from './assets/TCfooter.png';
+
+const App = () => {
+
+  return (
+    <div className='body'>
+      <img className='nav' src={TCnav} alt="Navigation" />
+      <img className='banner' src={TCbanner} alt="Banner" />
+      
+      <img className='footer' src={TCfooter} alt="Footer" />
+    </div>
+  );
+};
+
+export default App;
